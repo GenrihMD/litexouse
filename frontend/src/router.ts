508132c6edcfb8e307a1066cb0b85041c6e5db6a
alexanderl <alexanderl@kmslh.com>
2024-01-15 10:10:56 +0300

    Add router

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -11,7 +11,7 @@ const routes = [
         component: OutlineEdit
     }, {
         path: '/item/edit/data',
-        name: 'outline',
+        name: 'data',
         component: DataEdit
     }
 ];
